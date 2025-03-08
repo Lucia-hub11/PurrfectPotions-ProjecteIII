45d2d354be042be7b6e73643e81ca826fe377297
Lucia-hub11 <lucia.macias@estudiantat.upc.edu>
2025-03-08 12:19:46 +0100

    movement y camara

--- Assets/Scripts/Player And Camera/FollowPlayer.cs
+++ Assets/Scripts/Player And Camera/FollowPlayer.cs
@@ -4,15 +4,11 @@ using UnityEngine;
 
 public class FollowPlayer : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+   public Transform target;
+   public Vector3 offset;
 
-    // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
-        
+        transform.position = target.position + offset;
     }
 }
