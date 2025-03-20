f5b54d1a2b11bddfcbe93e155c770ea750be9059
Lucia-hub11 <lucia.macias@estudiantat.upc.edu>
2025-03-20 13:34:21 +0100

    Creo script ficha

---
+++ Assets/Scripts/Fiches.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Fiches : MonoBehaviour
+{
+    [SerializeField] private Image iconoFicha;
+
+    public Sprite iconoGeneral;
+    public Sprite iconoSeta;
+
+    public bool isSelected;
+}
