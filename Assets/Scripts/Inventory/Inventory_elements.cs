ab8290278a7a630f2a8e1442e5520aaaf59de4cc
anastasiaanaya <anastasia.anaya@estudiantat.upc.edu>
2025-03-25 14:31:51 +0100

    inventaridragDrop 1

---
+++ Assets/Scripts/Inventory/Inventory_elements.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory_elements 
+{
+    private List<Item> itemList;
+
+    public Inventory_elements()
+    {
+        itemList = new List<Item>();
+
+    }
+    public void AddItem(Item item)
+    {
+
+    }
+}
