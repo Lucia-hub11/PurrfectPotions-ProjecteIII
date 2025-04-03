c70ba2c34601f8f9efa52e94b7416b08f0835133
nora-pastoret <nora.pastoret@estudiantat.upc.edu>
2025-04-03 14:00:32 +0200

    petit arreglo

--- Assets/Scripts/Inventory/IngredientSlot.cs
+++ Assets/Scripts/Inventory/IngredientSlot.cs
@@ -6,16 +6,14 @@ using UnityEngine.UI;
 public class IngredientSlot : MonoBehaviour
 {
     public string ingredientName;
-    public Sprite itemSprite; //si l'altra es desto pq aquesta?
     public bool isFull;
 
     [SerializeField]
-    private Image itemImage; //en teoria la q apareix?
+    private Image itemImage; //la que apareix
 
     public void AddIngredientSprite(string ingredientName, Sprite itemSprite)
     {
         this.ingredientName = ingredientName;
-        this.itemSprite = itemSprite;
         isFull = true;
         itemImage.sprite = itemSprite;
         itemImage.enabled = true;
