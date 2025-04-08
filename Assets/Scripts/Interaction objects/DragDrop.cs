767887af72f24722d3d8fc1543d3b33957064962
Lucia-hub11 <lucia.macias@estudiantat.upc.edu>
2025-04-08 17:16:11 +0200

    raycasts

--- Assets/Scripts/Interaction objects/DragDrop.cs
+++ Assets/Scripts/Interaction objects/DragDrop.cs
@@ -3,45 +3,54 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class DragDrop : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, IEndDragHandler, IDragHandler, IDropHandler
+public class DragDrop : MonoBehaviour, IBeginDragHandler, IEndDragHandler, IDragHandler
 {
-    [SerializeField] private Canvas canvas;
-
-    private RectTransform rectTransform;
-    private CanvasGroup canvasGroup;
+    [SerializeField] private Canvas canvas;  // El canvas que contiene el objeto UI
+    private RectTransform rectTransform;     // RectTransform del objeto UI
+    private CanvasGroup canvasGroup;         // Para cambiar la opacidad y bloquear raycasts
+    private Vector3 originalPosition;        // Posición original para revertir si no se hace el drop
 
     private void Awake()
     {
         rectTransform = GetComponent<RectTransform>();
         canvasGroup = GetComponent<CanvasGroup>();
     }
+
     public void OnBeginDrag(PointerEventData eventData)
     {
-        Debug.Log("OnBeginDrag");
-        canvasGroup.alpha = .6f;
-        canvasGroup.blocksRaycasts = false;
+        // Guardamos la posición original del objeto para regresarlo si no se hace el drop en un objeto 3D
+        originalPosition = rectTransform.position;
+        canvasGroup.alpha = 0.6f;  // Reducimos la opacidad al arrastrar
+        canvasGroup.blocksRaycasts = false;  // Desactivamos raycasts mientras se está arrastrando
     }
 
     public void OnDrag(PointerEventData eventData)
     {
-        Debug.Log("OnDrag");
-        rectTransform.anchoredPosition += eventData.delta / canvas.scaleFactor;
+        // Convertimos la posición del ratón en coordenadas locales dentro del Canvas
+        Vector2 position;
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(canvas.GetComponent<RectTransform>(), eventData.position, canvas.worldCamera, out position);
+        rectTransform.localPosition = position;  // Actualizamos la posición del objeto en el Canvas
     }
 
     public void OnEndDrag(PointerEventData eventData)
     {
-        Debug.Log("OnEndDrag");
-        canvasGroup.alpha = 1f;
-        canvasGroup.blocksRaycasts = true;
-    }
+        canvasGroup.alpha = 1f;  // Restauramos la opacidad
+        canvasGroup.blocksRaycasts = true;  // Volvemos a activar los raycasts
 
-    public void OnPointerDown(PointerEventData eventData)
-    {
-        Debug.Log("OnPointerDown");
-    }
+        // Ahora calculamos el raycast en el mundo 3D al soltar el objeto
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);  // Creamos un ray desde la cámara hacia el ratón
+        RaycastHit hit;
 
-    public void OnDrop(PointerEventData eventData)
-    {
-        throw new System.NotImplementedException();
+        if (Physics.Raycast(ray, out hit))  // Si el raycast toca un objeto 3D
+        {
+            // Aquí convertimos la posición del mouse (pantalla) en un punto en el mundo 3D
+            rectTransform.position = hit.point;  // Colocamos el objeto UI en la posición del impacto
+            Debug.Log("Objeto 3D tocado: " + hit.collider.gameObject.name);  // Opcional: para verificar qué objeto fue tocado
+        }
+        else
+        {
+            // Si no tocó ningún objeto 3D, regresamos el objeto UI a su posición original
+            rectTransform.position = originalPosition;
+        }
     }
 }
