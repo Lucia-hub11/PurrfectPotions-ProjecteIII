769d6196e63c9aa69e27e2c872ca17fa5171cdb4
nora-pastoret <nora.pastoret@estudiantat.upc.edu>
2025-04-18 19:01:19 +0200

    missatge juno trist trevol

--- Assets/Scripts/Inventory/IngredientSlot.cs
+++ Assets/Scripts/Inventory/IngredientSlot.cs
@@ -7,11 +7,14 @@ public class IngredientSlot : MonoBehaviour
 {
     public string ingredientName;
     public bool isFull;
+    public GameObject CloverThoughts;
 
     [SerializeField]
     private Image itemImage; //la que apareix
 
     public GameObject ObtainedText;
+
+    private Sprite spriteWait;
 
     void Start()
     {
@@ -20,12 +23,33 @@ public class IngredientSlot : MonoBehaviour
 
     public void AddIngredientSprite(string ingredientName, Sprite itemSprite)
     {
-        
         this.ingredientName = ingredientName;
+        if (ingredientName == "Tear")
+        {
+            CloverThoughts.SetActive(true);
+            spriteWait = itemSprite;
+            Invoke("RegularAddSprite", 2f);
+
+        }
+        else
+        {
+            RegularAdd(itemSprite);
+        }
+    }
+
+    private void RegularAddSprite()
+    {
+        RegularAdd(spriteWait);
+    }
+
+
+    private void RegularAdd(Sprite itemSprite)
+    {
         isFull = true;
         itemImage.sprite = itemSprite;
         itemImage.enabled = true;
         itemImage.gameObject.SetActive(true);
+        CloverThoughts.SetActive(false);
         ObtainedText.SetActive(true);
         itemImage.enabled = true;
         itemImage.canvasRenderer.SetAlpha(1f);
