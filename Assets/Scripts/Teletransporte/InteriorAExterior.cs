392f3c710240dfe49200da7bc0c117f0a4f692fe
anastasiaanaya <154979891+anastasiaanaya@users.noreply.github.com>
2025-04-24 14:31:30 +0200

    canviCàmeres

--- Assets/Scripts/Teletransporte/InteriorAExterior.cs
+++ Assets/Scripts/Teletransporte/InteriorAExterior.cs
@@ -9,13 +9,25 @@ public class InteriorAExterior : MonoBehaviour
     public GameObject IngredientsInventory;
     public GameObject ObjectsInventory;
 
+    public CameraSwitcher cameraSwitcher;
+
     void OnTriggerEnter(Collider other){
 	  if(other.CompareTag("Player")){
 		  playerGO.SetActive(false);
 		  player.position = destination.position;
 		  playerGO.SetActive(true);
-            IngredientsInventory.SetActive(false);
-            ObjectsInventory.SetActive(true);
+          //IngredientsInventory.SetActive(false);
+          //ObjectsInventory.SetActive(true);
+
+            if (cameraSwitcher != null)
+            {
+                Debug.Log("Cambiando a MainCamera");
+                cameraSwitcher.SwitchToMain();
+            }
+            else
+            {
+                Debug.LogWarning("InteriorAExterior: cameraSwitcher no asignado");
+            }
         }
-  }
+    }
 }
