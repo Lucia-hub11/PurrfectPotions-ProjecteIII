875aaa6070c1df1fe1f430aa2b814391f8d1e5f1
anastasiaanaya <154979891+anastasiaanaya@users.noreply.github.com>
2025-04-25 23:21:13 +0200

    UIbuttons

---
+++ Assets/Scripts/UI/Buttons.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Buttons : MonoBehaviour
+{
+    public GameObject inventoryCanvas;
+    public GameObject mapCanvas;
+
+    private void Start()
+    {
+        if(inventoryCanvas==null || mapCanvas == null)
+        {
+            Debug.LogError("Inventory o Map canvas no estan asignados");
+        }
+        inventoryCanvas.SetActive(false);
+        mapCanvas.SetActive(false);
+    }
+
+    public void ToggleInventory()
+    {
+        //bool isOpen = inventoryCanvas.activeSelf;
+        //inventoryCanvas.SetActive(!isOpen);
+
+        ////Si se acaba de abrir el inventario, se cierra el mapa
+        //if(!isOpen)
+        //{
+        //    mapCanvas.SetActive(false);
+        //}
+        bool wasOpen = inventoryCanvas.activeSelf;
+        bool newState = !wasOpen;
+
+        // Aplicar estados
+        inventoryCanvas.SetActive(newState);
+        mapCanvas.SetActive(false);
+
+        Debug.Log("No es detecta el Inventory Canvas");
+    }
+
+    public void ToggleMap()
+    {
+        //bool isOpen = mapCanvas.activeSelf;
+        //mapCanvas.SetActive(!isOpen);
+
+        ////Si se acaba de abrir el mapa, se cierra el inventario
+
+        //if (!isOpen)
+        //{
+        //    inventoryCanvas.SetActive(false);
+        //}
+
+        bool wasOpen = mapCanvas.activeSelf;
+        bool newState = !wasOpen;
+
+        // Aplicar estados
+        mapCanvas.SetActive(newState);
+        inventoryCanvas.SetActive(false);
+
+        Debug.Log("No detecta el canvasMap");
+    }
+
+    //public void ShowInventory()
+    //{
+    //    inventoryCanvas.SetActive(true);
+    //    mapCanvas.SetActive(false);
+    //}
+    //public void ShowMap()
+    //{
+    //    mapCanvas.SetActive(true);
+    //    inventoryCanvas.SetActive(false);
+    //}
+}
