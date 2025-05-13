4ed5edb716c0aa02e8f9790b023b859b38037c64
Lucia-hub11 <lucia.macias@estudiantat.upc.edu>
2025-05-13 14:13:05 +0200

    seguint el tutorial

--- Assets/Scripts/UI/Lock.cs
+++ Assets/Scripts/UI/Lock.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -12,10 +13,64 @@ public class Lock : MonoBehaviour
     public string Password;
     public string[] LockCharacterChoices;
     public int[] _lockCharacterNumber;
-    public string _instertedPassword;
+    public string _insertedPassword;
 
     void Start()
     {
-        
+        _lockCharacterNumber = new int[Password.Length];
+    }
+    public void ChangePassword(int number)
+    {
+        _lockCharacterNumber[number]++;
+        if(_lockCharacterNumber[number] >=LockCharacterChoices[number].Length)
+        {
+            _lockCharacterNumber[number] = 0;
+        }
+        CheckPassword();
+        UpdateUI();
+    }
+
+    private void CheckPassword()
+    {
+        int pass_len = Password.Length;
+        _insertedPassword = "";
+        for(int i=0;i<pass_len;i++)
+        {
+            _insertedPassword += LockCharacterChoices[i][_lockCharacterNumber[i]].ToString();
+        }
+        if(Password == _insertedPassword)
+        {
+            Unlock();
+        }
+    }
+    public void Unlock()
+    {
+        Debug.Log("Unlocked");
+    }
+
+    private void UpdateUI()
+    {
+        int len = Text.Length;
+        for(int i = 0;i<len;i++)
+        {
+            Text[i].text = LockCharacterChoices[i][_lockCharacterNumber[i]].ToString();
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        Interact();
+    }
+
+    public void Interact()
+    {
+        if(Interactable)
+        {
+            LockCanvas.SetActive(true);
+        }
+    }
+    public void StopInteract()
+    {
+        LockCanvas.SetActive(false);
     }
 }
