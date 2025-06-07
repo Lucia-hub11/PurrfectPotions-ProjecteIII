b27f5b2668ff613230fcddaf04bddf188f16c906
Lucia-hub11 <lucia.macias@estudiantat.upc.edu>
2025-06-07 17:12:38 +0200

    en vez de canvas nueva escena para pasar de nivel

--- Assets/Scripts/Interaction objects/PotionManager.cs
+++ Assets/Scripts/Interaction objects/PotionManager.cs
@@ -1,32 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PotionManager : MonoBehaviour
 {
     int contadorIngredients = 0;
-    public GameObject PotionCanvas;
-    public GameObject BottomIngredientCanvas;
-    public GameObject CanvasHints;
-    public GameObject InventoriCanvas;
-    public GameObject CanvasBotons;
+   // public GameObject PotionCanvas;
+ //   public GameObject BottomIngredientCanvas;
+   // public GameObject CanvasHints;
+ //   public GameObject InventoriCanvas;
+   // public GameObject CanvasBotons;
 
-    void Start()
-    {
-        PotionCanvas.SetActive(false);
-    }
+  //  void Start()
+    //{
+  //      PotionCanvas.SetActive(false);
+    //}
     public void IngredientPotion()
     {
         contadorIngredients++;
         Debug.Log("Ingredientes en el caldero: " + contadorIngredients);
-        if(contadorIngredients == 3)
+        if(contadorIngredients == 1)
         {
-            PotionCanvas.SetActive(true);
-            BottomIngredientCanvas.SetActive(false);
-            CanvasHints.SetActive(false);
-            InventoriCanvas.SetActive(false);
-            CanvasBotons.SetActive(false);
-            Debug.Log("hola la poción ya está hecha que pasa??");
+     //       PotionCanvas.SetActive(true);
+       //     BottomIngredientCanvas.SetActive(false);
+         //   CanvasHints.SetActive(false);
+           // InventoriCanvas.SetActive(false);
+       //     CanvasBotons.SetActive(false);
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
         }
     }
 }
